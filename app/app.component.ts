ed664b058b401ddea4b47599c17ae4cd75176c99
Emilio <emiliocortina@gmail.com>
2019-07-25 17:16:10 +0200

    Clearing repo

--- app/app.component.ts
+++
@@ -1,7 +0,0 @@
-import { Component } from "@angular/core";
-
-@Component({
-  selector: "gr-app",
-  template: "<page-router-outlet></page-router-outlet>"
-})
-export class AppComponent { }
--- app/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformNativeScriptDynamic } from "nativescript-angular/platform";
-import { enableProdMode } from "@angular/core";
-
-import { AppModule } from "./app.module";
-
-enableProdMode();
-platformNativeScriptDynamic().bootstrapModule(AppModule);
