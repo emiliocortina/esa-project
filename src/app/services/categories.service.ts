e770e9eab37f9080b222e48b41f5aad0d89fa1c3
PJ <pacojuanquiros@gmail.com>
2019-08-03 15:14:44 +0200

    Logo and dummy post

--- src/app/services/categories.service.ts
+++ src/app/services/categories.service.ts
@@ -18,10 +18,10 @@ export class CategoriesService
   // TODO maybe take categories from database or json file
   private setUpCategories() : void
   {
-    this.addCategory('temp', 'Temperatures', '/assets/icon/hot.svg', 'assets/images/desert.jpg');
+    this.addCategory('temperatures', 'Temperatures', '/assets/icon/hot.svg', 'assets/images/desert.jpg');
     this.addCategory('pollution', 'Pollution', '/assets/icon/factory.svg', 'assets/images/pollution.jpg');
     this.addCategory('rain', 'Rain', '/assets/icon/drop.svg', 'assets/images/rain.jpg');
-    this.addCategory('tide', 'Tides', '/assets/icon/wave.svg', 'assets/images/tides.jpg');
+    this.addCategory('tides', 'Tides', '/assets/icon/wave.svg', 'assets/images/tides.jpg');
   }
 
   private addCategory(id: string, name: string, iconUrl: string, coverImageUrl: string) : void
--- src/app/services/topics.service.ts
+++ src/app/services/topics.service.ts
@@ -1,16 +1,20 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Topic} from './models/topic.model';
+import { CategoriesService } from './categories.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class TopicsService {
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private categoriesService: CategoriesService){
     }
 
     public loadPopularTopics(topics: Topic[]) {
+
+        topics.push(this.addDummy("We're doomed", "tides", "We're gonna die"));
+
         this.http.get('http://192.168.0.11:4567/api/popular')
             .subscribe((res) => this.processTopics(topics, res), (err) => console.log(err));
     }
@@ -19,5 +23,12 @@ export class TopicsService {
         for (const topic of res.topics) {
             topics.push(new Topic(topic.title, topic.category.name, topic.teaser.content));
         }
+    }
+
+
+    // TODO remove this
+    private addDummy(name: string, catId: string, teaser: string) : Topic
+    {
+        return new Topic(name, this.categoriesService.getCategory(catId), teaser);
     }
 }
