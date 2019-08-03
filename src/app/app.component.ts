f848e5dd79b337cc34105d72ad0a5f40ab34f0c2
PJ <pacojuanquiros@gmail.com>
2019-08-03 18:54:21 +0200

    Splash screen

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,23 +4,31 @@ import {Platform} from '@ionic/angular';
 import {SplashScreen} from '@ionic-native/splash-screen/ngx';
 import {StatusBar} from '@ionic-native/status-bar/ngx';
 
+import { timer } from 'rxjs';
+
 @Component({
     selector: 'app-root',
     templateUrl: 'app.component.html'
 })
 export class AppComponent {
+
+    showSplash: boolean = true;
+
     constructor(
         private platform: Platform,
         private splashScreen: SplashScreen,
         private statusBar: StatusBar
     ) {
         this.initializeApp();
+        
     }
 
     initializeApp() {
         this.platform.ready().then(() => {
             this.statusBar.styleDefault();
             this.splashScreen.hide();
+
+            timer(3000).subscribe(() => this.showSplash = false);
         });
     }
 
