a3c35d92002a6bd6e73908b66f60f5d5510494f8
Emilio <emiliocortina@gmail.com>
2019-08-12 15:50:43 +0200

    Added login page

--- client/src/app/tabs/profile/profile.module.ts
+++ client/src/app/tabs/profile/profile.module.ts
@@ -1,17 +1,20 @@
-import { IonicModule } from '@ionic/angular';
-import { RouterModule } from '@angular/router';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
-import { ProfilePage } from './profile.page';
+import {IonicModule} from '@ionic/angular';
+import {RouterModule} from '@angular/router';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {ProfilePage} from './profile.page';
+import {LoginPage} from './login/login.page';
 
 @NgModule({
-  imports: [
-    IonicModule,
-    CommonModule,
-    FormsModule,
-    RouterModule.forChild([{ path: '', component: ProfilePage }])
-  ],
-  declarations: [ProfilePage]
+    imports: [
+        IonicModule,
+        CommonModule,
+        FormsModule,
+        RouterModule.forChild([{path: '', component: ProfilePage},
+            {path: 'login', component: LoginPage}])
+    ],
+    declarations: [ProfilePage, LoginPage]
 })
-export class ProfilePageModule {}
+export class ProfilePageModule {
+}
