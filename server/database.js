06f69aa6479eac27fc5c7e195a01a529d7117abe
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-08-12 16:47:11 +0200

    ups

--- server/database.js
+++ server/database.js
@@ -1,8 +1,7 @@
 const mongoose = require('mongoose');
-const URI =
-	'mongodb+srv://admin:admin@glowingumbrella-7m46k.mongodb.net/GlowingUmbrella?retryWrites=true&w=majority';
+const URI = 'mongodb+srv://admin:admin@cooper-cluster-kjm9g.mongodb.net/test?retryWrites=true&w=majority';
 mongoose
-	.connect(URI,{ useNewUrlParser: true })
+	.connect(URI, { useNewUrlParser: true })
 	.then((db) => {
 		console.log('DB is connected');
 	})
