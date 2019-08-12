b6a01266da7b99df08e6c26e936e4683d6bd0947
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-08-12 17:03:08 +0200

    ups

--- client/src/app/tabs/profile/login/login.page.ts
+++ client/src/app/tabs/profile/login/login.page.ts
@@ -1,4 +1,9 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthenticationService } from 'src/app/services/authentication/authentication.service';
+import { LoginObject } from 'src/app/services/authentication/login-object';
+import { Session } from 'src/app/services/authentication/session';
+import { StorageService } from 'src/app/services/authentication/storage.service';
+import { Router } from '@angular/router';
 
 @Component({
     selector: 'app-login',
@@ -8,11 +13,39 @@ import {Component, OnInit} from '@angular/core';
 export class LoginPage implements OnInit {
 
     login: true;
+    submitted: boolean;
+    error: any;
 
-    constructor() {
+    constructor(private authenticationService: AuthenticationService, private storageService: StorageService, private router: Router) {
     }
 
     ngOnInit() {
+    }
+
+    public submitLogin(): void {
+
+        this.submitted = true;
+        this.error = null;
+        // TODO meter formulariode login
+        // if (this.loginForm.valid) {
+
+        //     this.authenticationService.login(new LoginObject(this.loginForm.value)).subscribe(
+        //         data => this.correctLogin(data),
+        //         error => this.errorLogin()
+        //     );
+        // }
+    }
+    errorLogin(): void {
+        console.log('Error en el login');
+    }
+
+    private correctLogin(data: Session) {
+        this.storageService.setCurrentSession(data);
+
+        this.error = null;
+        // this.router.navigate(['/home']);
+        this.router.navigate(['/']);
+
     }
 
 }
