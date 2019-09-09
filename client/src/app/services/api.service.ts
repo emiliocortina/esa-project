0b76d24ba7377eccc5fecb3532267e9d9438c371
Javier García Bermúdez <javier_garber@yahoo.es>
2019-09-09 21:43:56 +0200

    Arreglado acceso a la api

--- client/src/app/services/api.service.ts
+++ client/src/app/services/api.service.ts
@@ -9,158 +9,145 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { AuthenticationService } from './authentication/authentication.service';
 import { StorageService } from './authentication/storage.service';
 
-
-
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
-
 export class ApiService {
+	urlLit = 'http://localhost:3000/';
+	serverError: string;
 
-    urlLit = 'http://localhost:3000/api/';
-    serverError: string;
+	public expired = new BehaviorSubject<boolean>(false); // observable para en caso de error de sesion
 
-    public expired = new BehaviorSubject<boolean>(false); // observable para en caso de error de sesion
+	constructor(private httpClient: HttpClient, private router: Router, private storageService: StorageService) {}
 
-    constructor(
-        private httpClient: HttpClient,
-        private router: Router,
-        private authService: AuthenticationService,
-        private storageService: StorageService
-    ) { }
+	// DEPRECATED puede ser util en algun momento
+	// private handleError(error: any): Promise<any> {
+	//     // console.error('An error occurred', error); // for demo purposes only
+	//     if (error.error) {
+	//         if (error.error.details && error.error.details.length > 0) {
+	//             this.dialogService.error(this.extractErrorDetails(error.error.details),
+	//  error.error.errorDescription, MessageFormat.Html);
+	//         } else {
+	//             if (error.status == 403) {
+	//                 this.dialogService.error(error.message, 'Error');
+	//             } else {
+	//                 this.dialogService.error(error.error.errorDescription, 'Error');
+	//             }
+	//         }
+	//     }
+	//     return Promise.reject(error.message || error);
+	// }
 
-    // DEPRECATED puede ser util en algun momento
-    // private handleError(error: any): Promise<any> {
-    //     // console.error('An error occurred', error); // for demo purposes only
-    //     if (error.error) {
-    //         if (error.error.details && error.error.details.length > 0) {
-    //             this.dialogService.error(this.extractErrorDetails(error.error.details),
-    //  error.error.errorDescription, MessageFormat.Html);
-    //         } else {
-    //             if (error.status == 403) {
-    //                 this.dialogService.error(error.message, 'Error');
-    //             } else {
-    //                 this.dialogService.error(error.error.errorDescription, 'Error');
-    //             }
-    //         }
-    //     }
-    //     return Promise.reject(error.message || error);
-    // }
+	// private extractErrorDetails(details): string {
+	//     let result = '';
+	//     const table = '<table><tbody>#content#</tbody></table>';
 
-    // private extractErrorDetails(details): string {
-    //     let result = '';
-    //     const table = '<table><tbody>#content#</tbody></table>';
+	//     for (const det of details) {
+	//         result = `<tr><td><b>${det.errorField}:<b></td><td>${det.errorDescription}</td></tr>`;
+	//     }
 
-    //     for (const det of details) {
-    //         result = `<tr><td><b>${det.errorField}:<b></td><td>${det.errorDescription}</td></tr>`;
-    //     }
+	//     return table.replace('#content#', result);
+	// }
 
-    //     return table.replace('#content#', result);
-    // }
+	public setExpiredSession(expired: boolean = false) {
+		this.expired.next(expired);
+	}
 
-    public setExpiredSession(expired: boolean = false) {
-        this.expired.next(expired);
-    }
+	// Devuelve el valor de una cookie o null si la cookie no existe
+	public getCookie(name: string): string {
+		const nameLenPlus = name.length + 1;
+		return (
+			document.cookie
+				.split(';')
+				.map((c) => c.trim())
+				.filter((cookie) => {
+					return cookie.substring(0, nameLenPlus) === `${name}=`;
+				})
+				.map((cookie) => {
+					return decodeURIComponent(cookie.substring(nameLenPlus));
+				})[0] || null
+		);
+	}
 
-    // Devuelve el valor de una cookie o null si la cookie no existe
-    public getCookie(name: string): string {
-        const nameLenPlus = (name.length + 1);
-        return document.cookie
-            .split(';')
-            .map(c => c.trim())
-            .filter(cookie => {
-                return cookie.substring(0, nameLenPlus) === `${name}=`;
-            })
-            .map(cookie => {
-                return decodeURIComponent(cookie.substring(nameLenPlus));
-            })[0] || null;
-    }
+	private getHeaders(): HttpHeaders {
+		let headers = new HttpHeaders({
+			'Access-Control-Allow-Origin': '*',
+			'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT, OPTIONS',
+			'Content-Type': 'application/json',
+			'Accept-Language': 'es'
+		});
+		// DEPRECATED en algun momento se puede volver a esto
+		// const jwt = localStorage.getItem('jwt');
 
-    private getHeaders(): HttpHeaders {
+		const jwt = this.storageService.getCurrentToken();
+		if (jwt) {
+			headers = headers.append('Authorization', `Bearer ${jwt}`);
+		}
 
-        let headers = new HttpHeaders({
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, DELETE, PUT, OPTIONS',
-            'Content-Type': 'application/json',
-            'Accept-Language': 'es'
-        });
-        // DEPRECATED en algun momento se puede volver a esto
-        // const jwt = localStorage.getItem('jwt');
+		return headers;
+	}
 
-        const jwt = this.storageService.getCurrentToken();
-        if (jwt) {
-            headers = headers.append('Authorization', `Bearer ${jwt}`);
-        }
+	public buildSortAndFilterParamrs(fieldsToSort: SortField[], pageElements: number, pageNumber: number) {
+		const params: { sort_by: string; page_elements: number; page_number: number } = {
+			sort_by: '',
+			page_elements: 10,
+			page_number: 1
+		};
 
-        return headers;
-    }
+		if (fieldsToSort) {
+			let sortBy = '';
+			fieldsToSort.forEach((field) => {
+				sortBy += `${field.key}(${field.order}),`;
+			});
+			params.sort_by = sortBy;
+		}
+		if (pageElements) {
+			params.page_elements = pageElements;
+		}
+		if (pageNumber) {
+			params.page_number = pageNumber;
+		}
 
-    public buildSortAndFilterParamrs(fieldsToSort: SortField[], pageElements: number, pageNumber: number) {
-        const params: { sort_by: string, page_elements: number, page_number: number } = {
-            sort_by: '',
-            page_elements: 10,
-            page_number: 1
-        };
+		return params;
+	}
 
-        if (fieldsToSort) {
-            let sortBy = '';
-            fieldsToSort.forEach((field) => {
-                sortBy += `${field.key}(${field.order}),`;
+	request<R>(
+		service: string,
+		method: string,
+		params?: any,
+		body?: any,
+		showSpinner: boolean = true,
+		options?: RequestOptions
+	): Observable<R> {
+		const url = this.urlLit + service;
+		const complete = () => {};
 
-            });
-            params.sort_by = sortBy;
-        }
-        if (pageElements) {
-            params.page_elements = pageElements;
-        }
-        if (pageNumber) {
-            params.page_number = pageNumber;
-        }
+		return this.direct<R>(method, url, params, body, options).pipe(
+			tap(
+				(result) => {
+					complete();
+				},
+				(error) => {
+					complete();
+					// this.handleError(error);
+				}
+			)
+		);
+	}
 
-        return params;
-
-    }
-
-
-
-
-
-    request<R>(service: string, method: string, params?: any, body?: any, showSpinner: boolean = true,
-        options?: RequestOptions): Observable<R> {
-
-        const url = this.urlLit + service;
-        const complete = () => { };
-
-
-        return (
-            this.direct<R>(method, url, params, body, options))
-            .pipe(
-                tap(result => {
-                    complete();
-
-                }, error => {
-                    complete();
-                    // this.handleError(error);
-
-                })
-            );
-    }
-
-    private direct<R>(method: string, url: string, params: {}, body: {}, options: RequestOptions): Observable<R> {
-
-        const aux = this.getHeaders();
-        return this.httpClient.request<R>(method, url, {
-            headers: this.getHeaders(),
-            params,
-            body,
-            observe: 'response',
-        }).pipe(map((resp: HttpResponse<R>) => {
-
-            return resp.body;
-        }));
-    }
-
-
-
-
+	private direct<R>(method: string, url: string, params: {}, body: {}, options: RequestOptions): Observable<R> {
+		const aux = this.getHeaders();
+		return this.httpClient
+			.request<R>(method, url, {
+				headers: this.getHeaders(),
+				params,
+				body,
+				observe: 'response'
+			})
+			.pipe(
+				map((resp: HttpResponse<R>) => {
+					return resp.body;
+				})
+			);
+	}
 }
--- client/src/app/services/authentication/authentication.service.ts
+++ client/src/app/services/authentication/authentication.service.ts
@@ -1,41 +1,37 @@
-import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
-import {LoginObject} from './/login-object';
-import {SignUpObject} from './SignUpObject';
-import {Session} from './session';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { LoginObject } from './/login-object';
+import { SignUpObject } from './SignUpObject';
+import { Session } from './session';
 
-import {map} from 'rxjs/operators';
-import {HttpClient} from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { ApiService } from '../api.service';
 
 @Injectable({
-    providedIn: 'root'
+	providedIn: 'root'
 })
 export class AuthenticationService {
+	constructor(private http: HttpClient, private apiService: ApiService) {}
 
+	login(loginObj: LoginObject): Observable<Session> {
+		return this.apiService
+			.request('auth/login', 'post', null, loginObj)
+			.pipe(map((data) => this.extractData(data)));
+	}
 
-    constructor(private http: HttpClient) {
-    }
+	// tslint:disable-next-line:ban-types
+	logout(): Observable<Boolean> {
+		return this.apiService.request('auth/logout', 'post').pipe(map((data) => this.extractData(data)));
+	}
 
-    private basePath = 'http://192.168.0.11:3000/auth/';
+	signup(signUpObject: SignUpObject): Observable<Session> {
+		return this.apiService
+			.request('auth/signup', 'post', null, signUpObject)
+			.pipe(map((data) => this.extractData(data)));
+	}
 
-    login(loginObj: LoginObject): Observable<Session> {
-
-        return this.http.post(this.basePath + 'login', loginObj).pipe(map(data => this.extractData(data)));
-    }
-
-    // tslint:disable-next-line:ban-types
-    logout(): Observable<Boolean> {
-        return this.http.post(this.basePath + 'logout', {}).pipe(map(data => this.extractData(data)));
-    }
-
-    signup(signUpObject: SignUpObject): Observable<Session> {
-
-        return this.http.post(this.basePath + 'signup', signUpObject).pipe(map(data => this.extractData(data)));
-    }
-
-    private extractData(res: any) {
-        return res;
-    }
-
-
+	private extractData(res: any) {
+		return res;
+	}
 }
