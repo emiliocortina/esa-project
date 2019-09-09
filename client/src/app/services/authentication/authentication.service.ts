c72a978ea6e2cec3baf3c0ae3afe686c46689658
Javier García Bermúdez <javier_garber@yahoo.es>
2019-09-09 21:52:09 +0200

    Tslint

--- client/src/app/services/authentication/authentication.service.ts
+++ client/src/app/services/authentication/authentication.service.ts
@@ -19,7 +19,6 @@ export class AuthenticationService {
 			.request('auth/login', 'post', null, loginObj)
 			.pipe(map((data) => this.extractData(data)));
 	}
-
 	// tslint:disable-next-line:ban-types
 	logout(): Observable<Boolean> {
 		return this.apiService.request('auth/logout', 'post').pipe(map((data) => this.extractData(data)));
