4e999f27e341c4aeb8d0e596c92b90fe6ec7c10d
Javier Garcia Bermudez <javier_garber@yahoo.es>
2019-09-18 14:21:12 +0200

    borrado de la funcion falta

--- server/controllers/satelliteApi.controller.js
+++ server/controllers/satelliteApi.controller.js
@@ -4,62 +4,6 @@ const SatelliteRequestDto = require('./../models/rawSatelliteDataRequestObject')
 const errorServ = require('../services/error.service');
 const ramani = require('ramani');
 
-exports.getRawDataFromSatelliteApi = async (req, res, next) => {
-	let satRequestDto = SatelliteRequestDto.parseRequest(req.query);
-	console.log(req.query);
-	//TODO llamada de verdad
-	let satResponseDto = [
-		{
-			unit: 'L/m2',
-			dataPack: [
-				{ x: 0, y: 0.2 },
-				{ x: 1, y: 0.3 },
-				{ x: 2, y: 0.4 },
-				{ x: 3, y: 0.5 },
-				{ x: 4, y: 0.1 },
-				{ x: 5, y: 0.2 },
-				{ x: 6, y: 0.3 },
-				{ x: 7, y: 0.4 },
-				{ x: 8, y: 0.5 },
-				{ x: 9, y: 0.6 }
-			]
-		},
-		{
-			unit: 'mg/cm3',
-			dataPack: [
-				{ x: 0, y: 0.2 },
-				{ x: 1, y: 0.3 },
-				{ x: 2, y: 0.4 },
-				{ x: 3, y: 0.5 },
-				{ x: 4, y: 0.1 },
-				{ x: 5, y: 0.2 },
-				{ x: 6, y: 0.3 },
-				{ x: 7, y: 0.4 },
-				{ x: 8, y: 0.5 },
-				{ x: 9, y: 0.6 }
-			]
-		},
-		{
-			unit: 'ºC',
-			dataPack: [
-				{ x: 0, y: 0.2 },
-				{ x: 1, y: 0.3 },
-				{ x: 2, y: 0.4 },
-				{ x: 3, y: 0.5 },
-				{ x: 4, y: 0.1 },
-				{ x: 5, y: 0.2 },
-				{ x: 6, y: 0.3 },
-				{ x: 7, y: 0.4 },
-				{ x: 8, y: 0.5 },
-				{ x: 9, y: 0.6 }
-			]
-		}
-	];
-
-	res.status(HttpStatus.OK).json(satResponseDto);
-	return;
-};
-
 exports.handleDatasetCall = async (req, res, next) => {
 	console.log('Ha llegado a dataset');
 	return;
--- server/routes/satelliteApi.routes.js
+++ server/routes/satelliteApi.routes.js
@@ -2,9 +2,6 @@ const express = require('express');
 const router = express.Router();
 const ctrl = require('./../controllers/satelliteApi.controller');
 
-//router.post('/satelliteApi', ctrl.createSatelliteData);
-router.get('/satelliteApi', ctrl.getRawDataFromSatelliteApi);
-
 router.get('/dataset/*', ctrl.handleDatasetCall);
 router.get('/layer/*', ctrl.handleLayerCall);
 
