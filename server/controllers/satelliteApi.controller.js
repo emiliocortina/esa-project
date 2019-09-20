ee99859a640967da99cc4859d17c4dc1f59bdc97
pacojq <pacojuanquiros@gmail.com>
2019-09-20 15:58:40 +0200

    Bugs fixed

--- server/controllers/satelliteApi.controller.js
+++ server/controllers/satelliteApi.controller.js
@@ -176,37 +176,6 @@ exports.handleLayerCall = async (req, res, next) => {
 	return;
 };
 
-<<<<<<< HEAD
-exports.getZoneMap = async (req, res, next) => {
-	let satRequestDto = SatelliteRequestDto.parseRequest(req.query);
-	let lon = satRequestDto.longitude;
-	let lat = satRequestDto.latitude;
-	let halfSide = 0.03;
-	ramani.getArea(
-		// [
-		// 	[ lon - halfSide, lat + halfSide ],
-		// 	[ lon + halfSide, lat + halfSide ],
-		// 	[ lon - halfSide, lat - halfSide ],
-		// 	[ lon + halfSide, lat - halfSide ]
-		// ],
-		[
-			[ 52.382305628707854, 6.25396728515625 ],
-			[ 52.424196211696774, 6.372899355829467 ],
-			[ 52.30345857599569, 6.257838787287611 ],
-			[ 52.309025707071214, 6.638463891157568 ]
-		],
-		{
-			layer: 'ddl.GLOBALS2TRUECOLOR',
-			info_format: 'text/json',
-			return: 'aggregate'
-		},
-		function(error, result) {
-			console.log(error);
-			res.json(result);
-			return;
-		}
-	);
-=======
 exports.generateMap = async (req, res, next) => {
 	const len = 30;
 	const pattern = 'aA0';
@@ -257,5 +226,4 @@ exports.getMap = async (req, res, next) => {
 		return;
 	});
 	return;
->>>>>>> 7b181b080493edc80fd086c4a4bab358ef5689e2
 };
